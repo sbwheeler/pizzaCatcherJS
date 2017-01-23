86c2702854ff585d37bf3407e6bdefaeb9add753
Sam Wheeler <swheeler111@gmail.com>
2017-01-23 10:48:17 -0500

    heroku fixes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname + '/../')).listen(8080, function(){
+connect().use(serveStatic(__dirname + '/../')).listen(process.env.PORT || 8080, function(){
     console.log('Server running on 8080...');
 });
